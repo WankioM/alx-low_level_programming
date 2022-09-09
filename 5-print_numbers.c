b3a0b737f47e9d88d43b876aa8af803c8e3a6e9d
WankioM <111064857+WankioM@users.noreply.github.com>
2022-09-09 11:30:40 -0700

    Update 5-print_numbers.c

--- 5-print_numbers.c
+++ 5-print_numbers.c
@@ -1,7 +1,13 @@
-#include <stdlib.h>
-
-#include <time.h>
-
 #include <stdio.h>
+/**
+ * main - Prints numbers blow 10
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
 
 
