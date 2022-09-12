7b82b584f636ea863b3323088532079dfadec05b
WankioM <111064857+WankioM@users.noreply.github.com>
2022-09-12 15:26:20 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,29 +1,19 @@
-#include <stdio.h>
-
-
-
-/**
-
- * main - Prints "Programming is like building a multilingual
-
- *                puzzle, followed by a new line.
-
- *
-
- * Return: Always 0.
-
+/*
+ * File: 4-puts.c
+ * Writer: Tracy Wankio Marwa
  */
 
+#include <stdio.h>
 
-
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
-
 {
+	puts("\"Programming is like building a multilingual puzzle");
 
-puts("\"Programming is like building a multilingual puzzle");
-
-
-
-return (0);
-
+	return (0);
 }
