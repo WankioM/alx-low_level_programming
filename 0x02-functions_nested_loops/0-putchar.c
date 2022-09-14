cb31d8423506a996e0a762fad1c0ffd5a76514b7
WankioM <111064857+WankioM@users.noreply.github.com>
2022-09-14 10:24:45 -0700

    Update 0-putchar.c
    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,28 @@
+/*
+ * File: Putchar C
+ * Auth: Tracy Wankio Marwa
+ *
+ */
+
+#include "main.h"
+
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('P');
+	_putchar('u');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return (0);
+}
