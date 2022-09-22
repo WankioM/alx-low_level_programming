0037eab5a35060599c65f0aec4d963e76886c32b
WankioM <111064857+WankioM@users.noreply.github.com>
2022-09-22 14:08:04 -0700

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,1 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+*File: main.h
+*Auth: Tracy Wankio Marwa
+*Des: Header file containing all prototypes neeeded
+ *       used in the 0x05-pointers_arrays_strings directory.
+ */
+
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
