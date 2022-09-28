3892a97f54180dafa775f77f900c44a3e8599da1
WankioM <111064857+WankioM@users.noreply.github.com>
2022-09-28 09:59:49 -0700

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,19 @@
+/*
+ * File: 1-print_rev_recursion.c
+ * Auth: Tracy Wankio Marwa
+ */
 
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
