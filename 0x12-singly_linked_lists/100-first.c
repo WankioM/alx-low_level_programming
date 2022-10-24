ffe423fe39b64a978d7ad7f9a42ca8c13676c918
WankioM <111064857+WankioM@users.noreply.github.com>
2022-10-25 00:20:18 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,48 +4,15 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
+
+void __attribute__((constructor)) hare(void);
 
 /**
- * main - Prints the opcodes of itself.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
- *
- * Return: Always 0.
+ * hare - Prints a string before the
+ *        main function is executed.
  */
-int main(int argc, char *argv[])
+void hare(void)
 {
-	int bytes, index;
-	int (*address)(int, char **) = main;
-	unsigned char opcode;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		exit(1);
-	}
-
-	bytes = atoi(argv[1]);
-
-	if (bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-
-	for (index = 0; index < bytes; index++)
-	{
-		opcode = *(unsigned char *)address;
-		printf("%.2x", opcode);
-
-		if (index == bytes - 1)
-			continue;
-		printf(" ");
-
-		address++;
-	}
-
-	printf("\n");
-
-	return (0);
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
