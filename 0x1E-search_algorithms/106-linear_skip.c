4ed412a883ecc59145a27462d952d187aabd65bb
WankioM <111064857+WankioM@users.noreply.github.com>
2023-04-13 02:25:50 +0300

    Update 106-linear_skip.c

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,6 +1,6 @@
 /*
  * File: 106-linear_skip.c
- * Auth: Brennan D Baraban
+ * Auth: Tracy Wankio 
  */
 
 #include "search_algos.h"
@@ -8,14 +8,7 @@
 /**
  * linear_skip - Searches for an algorithm in a sorted singly
  *               linked list of integers using linear skip.
- * @list: A pointer to the  head of the linked list to search.
- * @value: The value to search for.
- *
- * Return: If the value is not present or the head of the list is NULL, NULL.
- *         Otherwise, a pointer to the first node where the value is located.
- *
- * Description: Prints a value every time it is compared in the list.
- *              Uses the square root of the list size as the jump step.
+ * @list: Means the headof the node.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
