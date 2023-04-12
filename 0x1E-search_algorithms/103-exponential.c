c608b7897cfd284e15308e826604688bdce75e97
WankioM <111064857+WankioM@users.noreply.github.com>
2023-04-13 02:30:28 +0300

    Update 103-exponential.c

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -6,18 +6,12 @@
 #include "search_algos.h"
 
 /**
-  * _binary_search - Searches for a value in a sorted array
-  *                  of integers using binary search.
-  * @array: A pointer to the first element of the array to search.
-  * @left: The starting index of the [sub]array to search.
-  * @right: The ending index of the [sub]array to search.
-  * @value: The value to search for.
-  *
-  * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the index where the value is located.
-  *
-  * Description: Prints the [sub]array being searched after each change.
-  */
+ * jump_list - Searches for an algorithm in a sorted singly
+ *             linked list of integers using jump search.
+ * @list: Means head
+ * @size: How many nodes
+ * @value: What we're looking for
+ */
 int _binary_search(int *array, size_t left, size_t right, int value)
 {
 	size_t i;
